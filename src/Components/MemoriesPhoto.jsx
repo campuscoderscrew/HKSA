be420ff03681a4800450b3f4e809ee955d419432
Carolina Herrera <ch.caro.her@gmail.com>
2025-08-30 20:57:53 -0400

    make homepage responsive

--- src/Components/MemoriesPhoto.jsx
+++ src/Components/MemoriesPhoto.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MemoriesPhoto = ({imageURL}) => {
     return (
-        <div class="w-[200px] h-[250px] border-solid border-white border-6 border-b-64">
+        <div class="border-solid border-white border-6 border-b-20 lg:border-b-64">
             <img className='w-full h-full' src={imageURL} alt="" />
         </div>
     );
